c713af752568611b1aceabc59695fd3b44b9140c
Pasi Lähteenmäki <decod81@gmail.com>
2018-04-10 09:16:55 +0300

    Add files via upload

--- MATLAB-FS-dithering.m
+++ MATLAB-FS-dithering.m
@@ -1,28 +1,28 @@
-%% Floyd-Steinberg dithering
 clear all
 close all
 
 M = 4;
-pixel = imread('david.png');
-figure; image(pixel); colormap(gray(256)); truesize;
-pixel = (M-1)*double(pixel)/255;
+pixel = imread('color.png');
+figure; image(pixel); truesize;
 
-[Y X] = size(pixel);
-for y = 2:Y-1
-    for x = 2:X-1
-        oldpixel = pixel(y, x);
-        newpixel = ceil(oldpixel);
-        pixel(y, x) = newpixel;
-        quant_error = oldpixel - newpixel;
-        pixel(y,   x+1) = pixel(y,   x+1) + quant_error*7/16;
-        pixel(y+1, x-1) = pixel(y+1, x-1) + quant_error*3/16;
-        pixel(y+1, x)   = pixel(y+1, x)   + quant_error*5/16;
-        pixel(y+1, x+1) = pixel(y+1, x+1) + quant_error*1/16;
+for z = 1:3
+    pix{z} = (M-1)*double(pixel(:, :, z))/255;
+    [Y X] = size(pix{z});
+    for y = 2:Y-1
+        for x = 2:X-1
+            oldpixel = pix{z}(y, x);
+            newpixel = round(oldpixel);
+            pix{z}(y, x) = newpixel;
+            quant_error = oldpixel - newpixel;
+            pix{z}(y,   x+1) = pix{z}(y,   x+1) + quant_error*7/16;
+            pix{z}(y+1, x-1) = pix{z}(y+1, x-1) + quant_error*3/16;
+            pix{z}(y+1, x)   = pix{z}(y+1, x)   + quant_error*5/16;
+            pix{z}(y+1, x+1) = pix{z}(y+1, x+1) + quant_error*1/16;
+        end
     end
+    img(:, :, z) = pix{z};
 end
-
 figure;
-image(uint8(255*pixel/(M-1)));
-colormap(gray(256));
+image(uint8(255*img/(M-1)));
 truesize;
-imwrite(uint8(255*pixel/(M-1)), 'MATLAB-FS-dithering-David.png'); 
+% imwrite(uint8(255*img/(M-1)), 'colors.png'); 
